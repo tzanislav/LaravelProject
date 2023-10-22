7ac22aadc99170d77cb4db2e223183a7a0cf5852
tzani <tzanislav@gmail.com>
2023-10-23 00:40:35 +0300

    Bug fix

--- resources/views/LogView.blade.php
+++ resources/views/LogView.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Logs</title>
+    <title>Logs View</title>
 </head>
 <body>
     <x-header data="Logs" />
@@ -27,6 +27,11 @@
         </tr>
         @endforeach
     </table>
+
+
+
+
+
 
 
 
